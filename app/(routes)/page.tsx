1f0c9dc5320399b33ad504fe80960824ee63df73
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-11-30 11:15:20 -0800

    revise a bit and add loading

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,6 +9,7 @@ export const revalidate = 0;
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("eaba6966-c636-4559-8e23-0524c7bb89a9");
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
